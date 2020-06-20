405edf1c1f4d3b36ffa6a8c86edadb23ea64abef
sharozraees <52934734+sharozraees802@users.noreply.github.com>
2020-06-20 17:56:30 +0500

    add hook and state

--- client/src/components/chat/Chat.jsx
+++ client/src/components/chat/Chat.jsx
@@ -1,9 +1,16 @@
 import React, { useState, useEffect } from "react";
 import querystring from "query-string";
-import io from 'socket.io-client';
+import io from "socket.io-client";
 //import './Chat.css';
 
-const Chat = () => {
+const Chat = ({ location }) => {
+  const [name, setName] = useState("");
+  const [room, setRoom] = useState("");
+  useEffect(() => {
+    const { name, room } = querystring.parse(location.search);
+    setName(name);
+    setRoom(room);
+  });
   return <h1>chat</h1>;
 };
 
