13443a39956c6eb98b61905c2284c111b7c23458
sharozraees <52934734+sharozraees802@users.noreply.github.com>
2020-06-21 16:00:02 +0500

    removeuser function

--- server/users.js
+++ server/users.js
@@ -17,7 +17,12 @@ const addUser = ({ id, name, room }) => {
   return { user };
 };
 
-const removeUser = () => {};
+const removeUser = (id) => {
+  const index = users.findIndex((user) => user.id === id);
+  if (index !== -1) {
+    return users.splice(index, 1)[0];
+  }
+};
 
 const getUser = () => {};
 
