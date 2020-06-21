87c721b77ce143eb6babf5b644807120dc9d1f29
sharozraees <52934734+sharozraees802@users.noreply.github.com>
2020-06-21 21:16:41 +0500

    adduser use in server.js

--- server/server.js
+++ server/server.js
@@ -2,20 +2,26 @@ const express = require("express");
 const socketio = require("socket.io");
 const http = require("http");
 
+const{ addUser, removeUser, getUser, getUserinRoom } = require('./users'); 
+
 const PORT = process.env.PORT || 5000;
 
 const router = require("./Router/router");
+const { use } = require("./Router/router");
 
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
 io.on("connection", (socket) => {
-  console.log("we have new connection");
-
   socket.on("join", ({ name, room }, callback) => {
-    console.log(name, room);
-  
+   const {error ,user}=addUser({id:socket.id,name,room});
+
+   if(error) return callback(error);
+  socket.emit('message',{user:'admin',text:`${user.name},welcome  to the room ${user.room}`});
+  socket.broadcast.to(user.room).emit('message',{user:'admin',text:`${user.name}, has joined!`});
+   socket.join(user.room);
+   callback()
   });
 
   socket.on("discoonect", () => {
