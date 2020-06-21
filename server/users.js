5dbffacd27ae233f31b1a2d1a54737cb58d22a06
sharozraees <52934734+sharozraees802@users.noreply.github.com>
2020-06-21 15:49:54 +0500

    add user function create

--- server/users.js
+++ server/users.js
@@ -1,26 +1,24 @@
 const users = [];
 
+const addUser = ({ id, name, room }) => {
+  name = name.trim().toLowercase();
+  room = room.trim().toLowercase();
+  const existingUser = users.find(
+    (user) => user.room === room && user.name === name
+  );
 
-const addUser = ({id,name,room})=>{
-    
+  if (existingUser) {
+    return { error: "Username is taken" };
+  }
 
+  const user = { id, name, room };
 
-}
+  users.push(user);
+  return { user };
+};
 
-const removeUser = ()=>{
+const removeUser = () => {};
 
+const getUser = () => {};
 
-}
-
-const getUser = ()=>{
-
-
-}
-
-
-
-const getUserinRoom = ()=>{
-
-
-}
-
+const getUserinRoom = () => {};
