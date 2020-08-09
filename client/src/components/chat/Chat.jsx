b0ac3b2e60fe0b185374b1b55a7233de9daf0dff
sharozraees <52934734+sharozraees802@users.noreply.github.com>
2020-08-09 16:10:56 +0500

    user .js error solve and serveer js error solve

--- client/src/components/chat/Chat.jsx
+++ client/src/components/chat/Chat.jsx
@@ -1,12 +1,14 @@
 import React, { useState, useEffect } from "react";
 import querystring from "query-string";
 import io from "socket.io-client";
-//import './Chat.css';
+import "./Chat.css";
 
 let socket;
 const Chat = ({ location }) => {
   const [name, setName] = useState("");
   const [room, setRoom] = useState("");
+  const [message, setMessage] = useState("");
+  const [messages, setMessages] = useState([]);
   const ENDPOINT = "localhost:5000";
   useEffect(() => {
     const { name, room } = querystring.parse(location.search);
@@ -14,18 +16,39 @@ const Chat = ({ location }) => {
     setName(name);
     setRoom(room);
 
-    socket.emit("join", { name, room },()=>{
+    socket.emit("join", { name, room }, () => {});
 
-    });
-
-    return ()=>{
-      socket.emit('discoonect')
+    return () => {
+      socket.emit("discoonect");
       socket.off();
-    }
-
-    
+    };
   }, [ENDPOINT, location.search]);
-  return <h1>chat</h1>;
+  useEffect(() => {
+    socket.on("message", (message) => {
+      setMessages([...messages, message]);
+    });
+  }, [messages]);
+  //function for sending message
+  const sendMessagae = (event) => {
+    event.preventDefault();
+    if (message) {
+      socket.emit("sendMessage", message, () => setMessage(''));
+    }
+  };
+  console.log(message, messages);
+  return (
+    <div className="outerContainer">
+      <div className="container">
+        <input
+          value={message}
+          onChange={(event) => setMessage(event.target.value)}
+          onKeyPress={(event) =>
+            event.key === "Enter" ? sendMessagae(event) : null
+          }
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Chat;
